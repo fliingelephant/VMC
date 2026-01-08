05db6d22eae4cb5c5fc65fc7710b84fa8ac32206
周唤海 <albus.zhouhh@gmail.com>
2026-01-08 19:46:42 +0800

    Add independent-set sampler and correctness checks

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,13 @@
 """Utility helpers for VMC workflows."""
 from __future__ import annotations
 
+from VMC.utils.independent_set_sampling import (
+    IndependentSetSampler,
+    build_neighbor_arrays,
+    independent_set_violations,
+    occupancy_to_spin,
+    spin_to_occupancy,
+)
 from VMC.utils.vmc_utils import (
     build_dense_jac,
     build_dense_jac_from_state,
@@ -9,6 +16,11 @@ from VMC.utils.vmc_utils import (
 )
 
 __all__ = [
+    "IndependentSetSampler",
+    "build_neighbor_arrays",
+    "independent_set_violations",
+    "occupancy_to_spin",
+    "spin_to_occupancy",
     "build_dense_jac",
     "build_dense_jac_from_state",
     "flatten_samples",
